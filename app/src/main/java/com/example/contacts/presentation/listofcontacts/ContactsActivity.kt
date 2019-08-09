99dcf53d1ecea2da2cfee001c14eeb6583453d86
VarHoro <varhoro@mail.ru>
2019-08-09 17:04:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/contacts/presentation/listofcontacts/ContactsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.contacts.presentation.listofcontacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.contacts.R
+
+class ContactsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts)
+    }
+}
---
+++ app/src/main/java/com/example/contacts/presentation/listofcontacts/ContactsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.contacts.presentation.listofcontacts
+
+import androidx.lifecycle.ViewModel
+
+class ContactsViewModel : ViewModel(){
+
+}
